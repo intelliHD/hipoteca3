496b9ebf53dd4115717a0435fb8f4de05d621630
intelliHD <106635823+intelliHD@users.noreply.github.com>
2022-06-01 14:20:05 +0200

    Update Formulario.jsx

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -92,7 +92,19 @@ const Formulario = ({cantidad, setCantidad, plazo, setPlazo, setTotal,deposito,s
                                 <MenuItem value="5">5%</MenuItem>
                                 <MenuItem value="6">6%</MenuItem>
                                 <MenuItem value="7">7%</MenuItem>
-                                <MenuItem value="50">50%</MenuItem>
+                                <MenuItem value="8">8%</MenuItem>
+                                <MenuItem value="9">9%</MenuItem>
+                                <MenuItem value="10">10%</MenuItem>
+                                <MenuItem value="11">11%</MenuItem>
+                                <MenuItem value="12">12%</MenuItem>
+                                <MenuItem value="13">13%</MenuItem>
+                                <MenuItem value="14">14%</MenuItem>
+                                <MenuItem value="15">15%</MenuItem>
+                                <MenuItem value="16">16%</MenuItem>
+                                <MenuItem value="17">17%</MenuItem>
+                                <MenuItem value="18">18%</MenuItem>
+                                <MenuItem value="19">19%</MenuItem>
+                                <MenuItem value="20">20%</MenuItem>
                             </Select>
                         {/* <select 
                         onChange={(e) => onChange(e)}
@@ -107,7 +119,19 @@ const Formulario = ({cantidad, setCantidad, plazo, setPlazo, setTotal,deposito,s
                             <option value="5">5%</option>
                             <option value="6">6%</option>
                             <option value="7">7%</option>
-                            <option value="50">50%</option>
+                            <option value="8">8%</option>
+                            <option value="9">9%</option>
+                            <option value="10">10%</option>
+                            <option value="11">11%</option>
+                            <option value="12">12%</option>
+                            <option value="13">13%</option>
+                            <option value="14">14%</option>
+                            <option value="15">15%</option>
+                            <option value="16">16%</option>
+                            <option value="17">17%</option>
+                            <option value="18">18%</option>
+                            <option value="19">19%</option>
+                            <option value="20">20%</option>
                         </select> */}
                     </div>
                     <div>
